a29df2c2a923d654b986610c44e51d8b88daae49
sendkite <sendkite1@gmail.com>
2023-02-25 22:36:50 +0900

    added db-connection with typeORM

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,19 @@
+import { type } from 'os';
+import { Column, Entity, JoinTable, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Flavor } from './flavor.entity';
+
+@Entity('coffee')
 export class Coffee {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @JoinTable()
+  @ManyToMany(type => Flavor, (flavor) => flavor.coffees,)
   flavors: string[];
 }
