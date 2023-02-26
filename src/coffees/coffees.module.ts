370b631498349fe5b0f46a23541230af0e5e481d
sendkite <sendkite1@gmail.com>
2023-02-26 23:08:54 +0900

    prac of class inject

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,12 +6,20 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
-class MockCoffeeService {}
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
+  providers: [
+    {
+      provide: ConfigService,
+      useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService,
+    },
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}
